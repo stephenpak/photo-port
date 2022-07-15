4746adc16042235abca5b01839fb9d0334dcd951
Stephen Pak <stephenjpak@gmail.com>
2022-07-15 15:13:25 -0700

    7-15-22-3:13pm

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,21 +1,19 @@
+// __tests__/About.test.js
 import React from 'react';
+import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import About from '..';
-import { render, cleanup } from '@testing-library/react';
 
 afterEach(cleanup);
 
 describe('About component', () => {
-  // First Test
   it('renders', () => {
     render(<About />);
   });
-
-  // Second Test
+  
   it('matches snapshot DOM node structure', () => {
-    // render About
+    const { asFragment } = render(<About />);
+    
+    expect(asFragment()).toMatchSnapshot();
   });
-});
-
-const { asFragment } = render(<About />);
-expect(asFragment()).toMatchSnapshot();
+})
